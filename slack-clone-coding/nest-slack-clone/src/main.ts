437b3259fe922fd537ab248d6761412ff5ca37af
khsexk <kohyunsuk98@gmail.com>
2022-05-09 21:31:00 +0900

    webpack 적용

--- slack-clone-coding/nest-slack-clone/src/main.ts
+++ slack-clone-coding/nest-slack-clone/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Hot Reload
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
+
+  // Hot Reload 세팅
+  if(module.hot){
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
