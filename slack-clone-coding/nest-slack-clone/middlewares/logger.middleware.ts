8f49ee1baff5cc27363484c9bbb15992004c15d3
khsexk <kohyunsuk98@gmail.com>
2022-05-09 23:04:36 +0900

    미들웨어 생성

---
+++ slack-clone-coding/nest-slack-clone/middlewares/logger.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+
+// morgan 같은 미들웨어 생성
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
--- slack-clone-coding/nest-slack-clone/src/app.module.ts
+++ slack-clone-coding/nest-slack-clone/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from 'middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  // 미들웨어 configure
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
